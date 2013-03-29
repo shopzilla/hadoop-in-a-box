39bae476381c91f9a26ca60c465395d2361a3862
jerluc <jlucas@shopzilla.com>
2013-03-28 19:26:37 -0700

    *Cleaned up POMs to get ready for open-sourceage *Updated license header comments for open-sourceage

--- hadoop-repl/src/main/java/com/shopzilla/hadoop/HadoopREPL.java
+++ hadoop-repl/src/main/java/com/shopzilla/hadoop/HadoopREPL.java
@@ -1,6 +1,20 @@
 /**
- * Copyright (C) 2004 - 2012 Shopzilla, Inc. 
- * All rights reserved. Unauthorized disclosure or distribution is prohibited.
+ * Copyright 2012 Shopzilla.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  http://tech.shopzilla.com
+ *
  */
 
 package com.shopzilla.hadoop;
@@ -172,7 +186,9 @@ public class HadoopREPL extends REPL {
     protected String evaluate(final String input) throws ExitSignal {
         final String[] inputParts = input.split("\\s+", 2);
         final String command = inputParts[0].toLowerCase();
-        if (REPL_COMMANDS.containsKey(command)) {
+        if (command.isEmpty()) {
+            return "";
+        } else if (REPL_COMMANDS.containsKey(command)) {
             return REPL_COMMANDS.get(command).execute(command, inputParts.length == 2 ? inputParts[1].split("\\s+") : new String[0]);
         } else {
             return format("Unknown command \"%s\"", command);
